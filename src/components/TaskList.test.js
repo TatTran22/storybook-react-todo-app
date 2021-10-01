71f322533a9dfe841673eec78b2ad3a8b82b3291
Tat Tran <tat.tran@wisecommerce.io>
2021-10-01 23:41:42 +0700

    Wire in data

--- src/components/TaskList.test.js
+++ src/components/TaskList.test.js
@@ -1,11 +1,12 @@
 import { render } from '@testing-library/react';
+
 import { composeStories } from '@storybook/testing-react';
 
-import * as TaskListStories from './TaskList.stories';
-
-const { WithPinnedTasks } = composeStories(TaskListStories);
+import * as TaskListStories from './TaskList.stories'; //👈  Our stories imported here
 
 //👇 composeStories will process all information related to the component (e.g., args)
+const { WithPinnedTasks } = composeStories(TaskListStories);
+
 it('renders pinned tasks at the start of the list', () => {
   const { container } = render(<WithPinnedTasks />);
 
